bfc019388a85b15ffa23f5e9a67faf77315527ca
Zachary Chisholm <zac@Zacharys-MacBook-Pro.local>
2018-02-26 14:01:36 +0000

    Created an http server and added info to the README.

---
+++ hello.js
@@ -0,0 +1,8 @@
+const http = require('http')
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(8080);
+
+console.log('Server running on port 8080.')
