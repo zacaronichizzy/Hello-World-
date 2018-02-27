74d90ec1b890881ed786fa0467694b7b52f29fa1
Zachary Chisholm <zacharychisholm@gmail.com>
2018-02-27 11:07:11 +0000

    Allow PORT environment variable

--- hello.js
+++ hello.js
@@ -1,8 +1,10 @@
 const http = require('http')
 
+const port = Number(process.env.PORT) || 8080
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8080);
+}).listen(port);
 
-console.log('Server running on port 8080.')
+console.log('Server running on port ' + port)
